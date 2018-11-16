3a0a968456adbeed2ef7cb4944f6257f0670cf5b
Sheena Bonilla <sheena.myb@gmail.com>
2018-11-16 03:59:36 +0000

    cli structure

--- lib/cli/cli_class.rb
+++ lib/cli/cli_class.rb
@@ -1,1 +1,11 @@
-user types zipcode
+# ask user for zipcode
+# user types zipcode
+# first scrape => grabs first 15 and create job objects
+# display 5 jobs
+# user selects job from list to get more info
+# second scrape => display everything nicely
+  # attributes: type (part/full), description, wage...
+# conditional => Still interested? Type yes or no
+# give options to see next 5 jobs
+# set variable equal to last index number?
+#
