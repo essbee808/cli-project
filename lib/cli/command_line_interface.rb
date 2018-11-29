f6e896144c5bd45485493503cd3f77588049be20
Sheena Bonilla <sheena.myb@gmail.com>
2018-11-29 22:02:04 +0000

    create spaces in cli class

--- lib/cli/command_line_interface.rb
+++ lib/cli/command_line_interface.rb
@@ -1,10 +1,19 @@
 # ask user for zipcode
+
 # user types zipcode
+
 # first scrape => grabs first 15 and create job objects
+Scraper.scrape_index_page(index)
+
 # display 5 jobs at a time
+
 # user selects job from list to get more info
+
 # second scrape => display everything nicely
   # attributes: type (part/full), description, wage...
+  
 # conditional => Still interested? Type yes or no
+
 # give options to see next 5 jobs
+
 # set variable equal to last index number?
