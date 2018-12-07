40614b777ae17aa7d1f3daeabca66e2bd85fcb91
Sheena Bonilla <sheena.myb@gmail.com>
2018-12-07 01:52:40 +0000

    add gem definition to notes

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,16 +5,15 @@ require 'pry'
 class Scraper
   
   def get_page
-    html = open('https://www.indeed.com/jobs?q=&l=' + input)
+    zipcode = CommandLineInterface.gets_input
+    html = open('https://www.indeed.com/jobs?q=&l=')
     parsed_page = Nokogiri::HTML(html)
   end
   #need to set index_url to https://www.indeed.com/jobs?q=&l= and add zipcode at the end of url
 
-  def self.scrape_index_page(index_url)
-    html = open(index_url)
-    parsed_page = Nokogiri::HTML(html)
+  def self.scrape_index_page
+    get_page
     all_jobs = parsed_page.css() #=> look up CSS selector for job
-    
   end
   
 end
