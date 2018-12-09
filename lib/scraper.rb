84873ca03ec64533082bf9aff7bbf35f78fdc7e9
Sheena Bonilla <sheena.myb@gmail.com>
2018-12-09 01:30:21 +0000

    css selector for Scraper class

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,13 +8,13 @@ class Scraper
   def get_page(input)
     html = open("https://www.indeed.com/l-#{input}-jobs.html") # interpolate input
     parsed_page = Nokogiri::HTML(html)
-    #binding.pry
+    binding.pry
   end
   #need to set index_url to https://www.indeed.com/jobs?q=&l= and add zipcode at the end of url
 
   def self.scrape_index_page
     get_page
-    all_jobs = parsed_page.css() #=> look up CSS selector for job
+    all_jobs = parsed_page.css('div.jobsearch-SerpJobCard') #=> look up CSS selector for job
   end
   
 end
