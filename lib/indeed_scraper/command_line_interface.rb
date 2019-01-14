f0bcc1229154c4043f1886d7a5af21446db469ee
Sheena Bonilla <34547555+essbee808@users.noreply.github.com>
2019-01-13 21:43:27 -0800

    Remove comments from command line interface

--- lib/indeed_scraper/command_line_interface.rb
+++ lib/indeed_scraper/command_line_interface.rb
@@ -118,25 +118,4 @@ class CommandLineInterface
   def clear_all
     Job.all.clear
   end
-
 end
-            
-
-# prompt user for zipcode
-  #Great! Please enter your 5 digit zipcode:
-
-# user types zipcode
-
-# first scrape => grabs first 15 and create job objects
-#Scraper.scrape_index_page(index)
-
-# user selects job from list to get more info
-
-# second scrape => display everything nicely
-  # attributes: type (part/full), description, wage...
-  
-# conditional => Still interested? Type yes or no
-
-# give options to see next 5 jobs
-
-# set variable equal to last index number?
