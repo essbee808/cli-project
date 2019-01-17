e4cf429425fea3bb22741a5b21ed8b0922397d06
Sheena Bonilla <sheena.myb@gmail.com>
2019-01-16 21:08:30 -0800

    Add #select_number and refactor conditional statement in #make_selection

--- lib/indeed_scraper/command_line_interface.rb
+++ lib/indeed_scraper/command_line_interface.rb
@@ -2,17 +2,8 @@ class CommandLineInterface
   
   BASE_PATH = "https://www.indeed.com"
   
-  def run_program 
-      make_jobs
-      display_job
-      make_selection
-  end
-  
   def greeting
-    puts "Hello there! Welcome to Indeed Scraper Command Line Interface.\n\n".green
-    puts "                       /\\_/\\                     ".magenta.blink
-    puts "                      ( o.o )                       ".magenta.blink
-    puts "                       > ^ <                       \n\n".magenta.blink
+    puts "Hello there! Welcome to Indeed Scraper Command Line Interface.".green
     puts "What is your name?".green
     @user_name = user_input.upcase
     puts " "
@@ -20,13 +11,24 @@ class CommandLineInterface
     @user_name
   end
 
+  def run_program 
+    make_jobs
+    display_all_jobs
+    make_selection(@input)
+  end
+  
+  def zipcode
+    puts "Enter your 5 digit zipcode:".green
+  end
+
   def user_input
     input = gets.strip #remove white space
     @input = input
   end
 
-  def zipcode
-    puts "Enter your 5 digit zipcode:".green
+  def select_number
+    puts "Enter a number from the list for more info or type 'exit' to exit program:".green
+    user_input
   end
 
   def make_jobs
@@ -38,47 +40,49 @@ class CommandLineInterface
     Job.create_from_collection(jobs_array)  # creates an array of job objects with 5 attributes
   end
   
-  def make_selection
-    puts "Select a number from the list above or type 'new' to enter a new 5 digit zipcode:".green
-    user_input 
-    #=> conditional statement depending on user's selection
-    if (@input.to_i-1) <= Job.all.size
-        puts " "
-        job = Job.all[@input.to_i-1]
-        puts " "
-        puts "TITLE: ".blue + "#{job.title}\n" if !job.title.empty?        
-        puts "COMPANY: ".blue + "#{job.company}\n" if !job.company.empty?
-        puts "LOCATION: ".blue + "#{job.location}\n" if !job.location.empty?
-        puts "SALARY: ".blue + "#{job.salary}\n" if !job.salary.empty?
-        puts "DESCRIPTION: ".blue + "#{job.description}\n" if !job.description.empty?
-      menu
-      @url = BASE_PATH + job.job_url
-    elsif @input == "new"
-      Job.clear_all
-      run_program
+  def make_selection(input = nil)
+    if @input.to_i <= Job.all.size && @input.to_i != 0
+      #retrieve job object by index number
+      @job = Job.all[(@input.to_i)-1]
+        puts "TITLE: ".blue + "#{@job.title}\n" if !@job.title.empty?        
+        puts "COMPANY: ".blue + "#{@job.company}\n" if !@job.company.empty?
+        puts "LOCATION: ".blue + "#{@job.location}\n" if !@job.location.empty?
+        puts "SALARY: ".blue + "#{@job.salary}\n" if !@job.salary.empty?
+        puts "DESCRIPTION: ".blue + "#{@job.description}\n" if !@job.description.empty?
+      menu_list(@job.job_url)
     else
+      select_number
       make_selection
     end
   end
 
-  def menu
+  def menu_list(job_url)
     puts " "
     puts "What do you want to do, #{@user_name}?".green
     puts "Enter a number:\n".green
     puts "1. Learn more"
     puts "2. Go back to list"
-    puts "3. Exit\n\n"
+    puts "3. Enter new zipcode"
+    puts "4. Exit\n\n"
 
     user_input
+
     if @input.to_i == 1 #=> utilize #to_i to convert input to integer
-      puts "Still in progress..."
+      system("open", "#{BASE_PATH + @job.job_url}") #=> automatically open job posting via terminal
+      menu_list
     elsif @input.to_i == 2
-      display_job
-      make_selection
+      display_all_jobs
+      make_selection(@input)
     elsif @input.to_i == 3
+      Job.clear_all
+      run_program
+    elsif @input.to_i == 4
       puts "See you later, #{@user_name}! Good luck on your job search!".green
+      puts "                       /\\_/\\                     ".magenta.blink
+      puts "                      ( o.o )                       ".magenta.blink
+      puts "                       > ^ <                       \n\n".magenta.blink
     else
-      menu
+      menu_list
     end
   end
 
@@ -92,14 +96,11 @@ class CommandLineInterface
     end
     @input
   end
-
-  # def open_in_browser(job)
-  #   system("open BASE_PATH#{{job.job_url}}")
-  # end
   
-  def display_job
-      Job.all.each.with_index(1) do |el, index|
-            puts "#{index}" + ". " + "#{el.title}\n\n"
-      end
+  def display_all_jobs
+    Job.all.each.with_index(1) do |el, index|
+      puts "#{index}" + ". " + "#{el.title}\n\n"
+    end
+    select_number
   end
 end
